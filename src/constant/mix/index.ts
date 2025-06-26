d1ca4b30543958752fd572a24a9e627ce244cfe0
goldliondevx <eityeily@gmail.com>
2025-06-26 13:29:15 -1000

    feat:update for new contract address

--- src/constant/mix/index.ts
+++ src/constant/mix/index.ts
@@ -3,17 +3,17 @@ const MIX_CONFIG = {
         /**
          * Mixer contract address
          */
-        // MIXER_CONTRACT_ADDRESS: '0x8adE00BEdC6234463f34d18bE2A6028DEbB4F530', // Mainnet
-        MIXER_CONTRACT_ADDRESS: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', // Testnet
-        MIXER_PROGRAM_ID: '7grL6oHWcuwdBNkqCUrz7JEoHeS5NXv1FDegDr6ViMBi',
+        MIXER_CONTRACT_ADDRESS: '0xcAfe510F5E5d5712Bc8514C12FfCfb8d4928443f', // Mainnet
+        // MIXER_CONTRACT_ADDRESS: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', // Testnet
+        MIXER_PROGRAM_ID: '428L2unZJtHemoLLtaEAotJGk1Hkoj6nHp5tDUpmKJMw',
 
         /**
          * Mix token address
          */
-        // ETH_TOKEN: "0x686c5961370db7f14f57f5a430e05deae64df504", // Mainnet
-        // SOL_TOKEN: "BAezfVmia8UYLt4rst6PCU4dvL2i2qHzqn4wGhytpNJW" // Mainnet
-        ETH_TOKEN: '0x5FbDB2315678afecb367f032d93F642f64180aa3', //  Testnet
-        SOL_TOKEN: 'Bq2cu6o9bhdecFxD7pLbb3VMakbo9TAQgU8jUUzjCAh3'  // Testnet
+        ETH_TOKEN: "0x686c5961370db7f14f57f5a430e05deae64df504", // Mainnet
+        SOL_TOKEN: "BAezfVmia8UYLt4rst6PCU4dvL2i2qHzqn4wGhytpNJW" // Mainnet
+        // ETH_TOKEN: '0x5FbDB2315678afecb367f032d93F642f64180aa3', //  Testnet
+        // SOL_TOKEN: 'Bq2cu6o9bhdecFxD7pLbb3VMakbo9TAQgU8jUUzjCAh3'  // Testnet
     },
     // ETH2SOL_CURRENCY_MAP: { // Mainnet
     //     '0x686c5961370db7f14f57f5a430e05deae64df504': 'BAezfVmia8UYLt4rst6PCU4dvL2i2qHzqn4wGhytpNJW',
